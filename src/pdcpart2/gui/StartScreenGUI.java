eeb80e1c10bd0497902612962ca989ee12882942
Onafanewlevel <rosemicluvin@icloud.com>
2024-10-30 01:48:51 +1300

    updated to utilize GameControl interface

--- src/pdcpart2/gui/StartScreenGUI.java
+++ src/pdcpart2/gui/StartScreenGUI.java
@@ -19,7 +19,8 @@ import pdcpart2.util.FontLoader;
  *
  * Additionally, it displays a table of previous game results.
  *
- * Author: Setefano Muller Tharuka Rodrigo
+ * Author: Setefano Muller
+ *         Tharuka Rodrigo
  */
 public class StartScreenGUI extends JFrame implements GameControl {
 
